b6c8d5b7c6e36529405d1cbfd90bf52694badc49
Juanma Hidalgo <juanma06@gmail.com>
2025-05-12 18:02:20 +0200

    feat: export getOperation

--- src/polygon/modules/analytics.ts
+++ src/polygon/modules/analytics.ts
@@ -64,7 +64,7 @@ export function isThirdPartySale(buyer: string): boolean {
   return false;
 }
 
-function getOperation(buyer: string): Operation {
+export function getOperation(buyer: string): Operation {
   if (isTransakOperation(buyer)) {
     return Operation.fiat;
   } else if (isAxelarOperation(buyer)) {
